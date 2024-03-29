2d58cee7ff1bd50d207725950f067bebf81a7496
tk\_ALPHA <tk_alpha@nv.org>
2024-03-29 21:32:02 +0100

    login okay

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app:{
+    head: {
+      meta: [
+        {
+          "name": "viewport",
+          "content": "width=device-width, initial-scale=1"
+        },
+        {
+          "charset": "utf-8"
+        }
+      ],
+      script: [
+        { src: 'assets/js/pace.min.js' }
+      ],
+      link: [
+        { rel: 'stylesheet', href: 'assets/css/pace.min.css' },
+        { rel: 'stylesheet', href: 'assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css' },
+        { rel: 'stylesheet', href: 'assets/css/bootstrap.min.css' },
+        { rel: 'stylesheet', href: 'assets/css/bootstrap-extended.css' },
+        { rel: 'stylesheet', href: 'assets/css/style.css' },
+        { rel: 'stylesheet', href: 'assets/css/icons.css' },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap' },
+
+      ]
+    },
+  }
 })
---
+++ pages/login.vue
@@ -0,0 +1,78 @@
+<template>
+
+<body class="bg-white">
+
+  <!--start wrapper-->
+  <div class="wrapper">
+    <div class="">
+      <div class="row g-0 m-0">
+        <div class="col-xl-6 col-lg-12">
+          <div class="login-cover-wrapper">
+            <div class="card shadow-none">
+              <div class="card-body">
+                <div class="text-center">
+                  <h4>Sign In</h4>
+                  <p>Sign In to your account</p>
+                </div>
+                <form class="form-body row g-3">
+          
+                  <div class="col-12">
+                    <label for="inputEmail" class="form-label">Email</label>
+                    <input type="email" class="form-control" id="inputEmail">
+                  </div>
+                  <div class="col-12">
+                    <label for="inputPassword" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="inputPassword">
+                  </div>
+                  <div class="col-12 col-lg-6">
+                    <div class="form-check form-switch">
+                      <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckRemember">
+                      <label class="form-check-label" for="flexSwitchCheckRemember">Remember Me</label>
+                    </div>
+                  </div>
+                  <div class="col-12 col-lg-6 text-end d-none">
+                    <a href="authentication-reset-password-cover.html">Forgot Password?</a>
+                  </div>
+                  <div class="col-12 col-lg-12">
+                    <div class="d-grid">
+                      <button type="button" class="btn btn-primary">Sign In</button>
+                    </div>
+                  </div>
+                  <div class="col-12 col-lg-12 d-none">
+                    <div class="position-relative border-bottom my-3">
+                      <div class="position-absolute seperator translate-middle-y">or continue with</div>
+                    </div>
+                  </div>
+                  <div class="col-12 col-lg-12 d-none">
+                    <div class="social-login d-flex flex-row align-items-center justify-content-center gap-2 my-2">
+                      <a href="javascript:;" class=""><img src="assets/images/icons/facebook.png" alt=""></a>
+                      <a href="javascript:;" class=""><img src="assets/images/icons/apple-black-logo.png" alt=""></a>
+                      <a href="javascript:;" class=""><img src="assets/images/icons/google.png" alt=""></a>
+                    </div>
+                  </div>
+                  <div class="col-12 col-lg-12 text-center d-none">
+                    <p class="mb-0">Don't have an account? <a href="authentication-sign-up-cover.html">Sign up</a></p>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-6 col-lg-12">
+          <div class="position-fixed top-0 h-100 d-xl-block d-none login-cover-img">
+         
+          </div>
+        </div>
+
+        <img src="assets/logo.png" style="width:100px; bottom:90px; right:50px" class="position-absolute "/> 
+      </div>
+      <!--end row-->
+    </div>
+  </div>
+  <!--end wrapper-->
+
+
+</body>
+
+
+</template>
