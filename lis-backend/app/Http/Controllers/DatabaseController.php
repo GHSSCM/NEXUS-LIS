b7c231896dfe70a76cc1a7459bb7924214fba07d
TK\ Alpha <tclarencek@gmail.com>
2024-10-06 18:44:00 +0100

    complete import

--- lis-backend/app/Http/Controllers/DatabaseController.php
+++ lis-backend/app/Http/Controllers/DatabaseController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Symfony\Component\Process\Process;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 
@@ -36,7 +38,82 @@ class DatabaseController extends Controller
         return response()->download($filepath)->deleteFileAfterSend(true);
     }
 
+
     public function importDatabase(Request $request)
+    {
+        
+        
+      
+        // $request->validate([
+        //     'sql_file' => 'required|mimes:sql|max:10000'
+        // ], [
+        //     'sql_file.required' => 'Please upload an SQL file.',
+        //     'sql_file.mimes' => 'Only SQL files are allowed.',
+        //     'sql_file.max' => 'The uploaded file must not exceed 10MB.'
+        // ]);
+
+        
+        $databaseName = env('DB_DATABASE');
+        $username = env('DB_USERNAME');
+        $password = env('DB_PASSWORD');
+        $host = env('DB_HOST');
+        
+        $file = $request->file('sql_file');
+
+
+
+        $filepath = $file->storeAs('imports', now().'_'. $file->getClientOriginalName());
+
+
+        $isCompiledVersion =  !empty(env("COMPILED_VERSION"));
+
+
+        // DB::statement("DROP DATABASE IF EXISTS `$databaseName`");
+        // DB::statement("CREATE DATABASE IF NOT EXISTS `$databaseName`");
+
+
+        $path = !$isCompiledVersion? storage_path('app/' . $filepath): (storage_path('app\\'.str_replace("/","\\",$filepath)));
+
+
+        // echo $path;
+        $content  = file_get_contents($path);
+        // echo $content;
+        $content="DROP DATABASE IF EXISTS `$databaseName`; CREATE DATABASE IF NOT EXISTS `$databaseName`; USE `$databaseName`; ".$content;
+        DB::unprepared($content);
+
+        die("<div style='padding:40px;'><h1>Imported successfully. Please wait. Redirecting to login... </h1><script> window.localStorage.clear();setTimeout(function(){window.location='/login'},3000)</script></div>");
+
+
+        return;
+
+
+        $mysqlPath = $isCompiledVersion?"C:\\xampp\\mysql\\bin\\mysql":"mysql";
+
+        $command = "$mysqlPath --user={$username} ".(!empty($password)?"--password={$password}":"")." --host={$host} {$databaseName} -e \"DROP DATABASE IF EXISTS {$databaseName}; CREATE DATABASE {$databaseName};\"" ;
+        $process = Process::fromShellCommandline($command);
+
+        try {
+            $process->mustRun();
+        } catch (ProcessFailedException $exception) {
+            return response()->json(['error' => 'Database import failed[1]: ' . $exception->getMessage()], 500);
+        }
+
+
+
+
+        $command = "$mysqlPath --user={$username} ".(!empty($password)?"--password={$password}":"")." --host={$host} {$databaseName} < " . storage_path('app/' . $filepath);
+        $process = Process::fromShellCommandline($command);
+
+        try {
+            $process->mustRun();
+        } catch (ProcessFailedException $exception) {
+            return response()->json(['error' => 'Database import failed[2]: ' . $exception->getMessage()], 500);
+        }
+
+        die("<div style='padding:40px;'><h1>Imported successfully. Please wait. Redirecting to login... </h1><script>setTimeout(function(){window.location='/login'},3000)</script></div>");
+    }
+
+    /*   public function importDatabase(Request $request)
     {
         
         
@@ -87,5 +164,5 @@ class DatabaseController extends Controller
         }
 
         die("<div style='padding:40px;'><h1>Imported successfully. Please wait. Redirecting to login... </h1><script>setTimeout(function(){window.location='/login'},3000)</script></div>");
-    }
+    } */
 }
